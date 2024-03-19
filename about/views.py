d72277970da1e4e1696c530aa78ec9bca3369bf2
phildehovre <521153@waes.ac.uk>
2024-03-19 09:55:25 +0000

    add about views

--- about/views.py
+++ about/views.py
@@ -3,6 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    print(request)
-    return HttpResponse("<h1>Hello world!</h1>")
+def index(req):
+    return HttpResponse('<h1>About</h1>')
